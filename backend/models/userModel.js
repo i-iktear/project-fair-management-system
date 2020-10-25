72661df41fd3a3687a468fc066329ab94bbbad6d
Iktear Uddin <emon5aurk@gmail.com>
2020-10-25 15:32:35 +0600

    creating userModel

---
+++ backend/models/userModel.js
@@ -0,0 +1,64 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    universityId: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    isAdmin: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+    isStudent: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+    isTeacher: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+    isJudge: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+const User = mongoose.model("User", userSchema);
+
+export default User;

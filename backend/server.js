29142c2af224bbce42613e0bc44c650b3f621b51
Iktear Uddin <emon5aurk@gmail.com>
2020-12-10 09:19:06 +0600

    Added: session and projects to the server

--- backend/server.js
+++ backend/server.js
@@ -1,30 +1,37 @@
-import express from 'express'
-import dotenv from 'dotenv'
-import colors from 'colors'
-import connectDB from './config/db.js'
-import productRoutes from './routes/productRoutes.js'
-import userRoutes from './routes/userRoutes.js'
-import {notFound,errorHandler} from './middleware/errorMiddleware.js'
+import express from "express";
+import dotenv from "dotenv";
+import colors from "colors";
+import connectDB from "./config/db.js";
+import userRoutes from "./routes/userRoutes.js";
+import sessionRoutes from "./routes/sessionRoutes.js";
+import ProjectRoutes from "./routes/ProjectRoutes.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
-dotenv.config()
+dotenv.config();
 
-connectDB()
+connectDB();
 
-const app = express()
-app.use(express.json())
+const app = express();
+app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("Api is running.....");
+});
 
-app.get('/',(req,res) => {
-    res.send('Api is running.....')
-})
+app.use("/api/users", userRoutes);
+app.use("/api/sessions", sessionRoutes);
+app.use("/api/projects", ProjectRoutes);
 
-app.use('/api/products',productRoutes)
-app.use('/api/users',userRoutes)
+app.use(notFound);
 
-app.use(notFound)
+app.use(errorHandler);
 
-app.use (errorHandler)
+const PORT = process.env.PORT || 5000;
 
-const PORT = process.env.PORT || 5000
-
-app.listen(PORT, console.log(`Server is running in ${process.env.NODE_ENV} on Port ${PORT}`.yellow.underline.bold))
+app.listen(
+  PORT,
+  console.log(
+    `Server is running in ${process.env.NODE_ENV} on Port ${PORT}`.yellow
+      .underline.bold
+  )
+);

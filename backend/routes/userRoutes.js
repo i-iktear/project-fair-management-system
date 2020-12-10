809c6f9c9160756e8de7508a078512f3223c56fc
Iktear Uddin <emon5aurk@gmail.com>
2020-12-10 09:16:45 +0600

    Added: more user routes for moderator

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -5,14 +5,31 @@ import {
   getUserProfile,
   registerUser,
   updateUserProfile,
+  getUsers,
+  getUserById,
+  deleteUser,
+  updateUser,
+  updateUserByModerator,
 } from "../controllers/userController.js";
-import { protect } from "../middleware/authMiddlware.js";
+import { protect, admin, moderator } from "../middleware/authMiddlware.js";
 
 router.post("/login", authUser);
 router
   .route("/profile")
   .get(protect, getUserProfile)
   .put(protect, updateUserProfile);
-router.route("/").post(registerUser);
+router.route("/").post(registerUser).get(protect,getUsers);
+
+router
+  .route("/:id")
+  .delete(protect, admin, deleteUser)
+  .get(protect, admin, getUserById)
+  .put(protect, admin, updateUser);
+
+router.route("/moderator").get(protect, moderator, getUsers);
+router
+  .route("/moderator/:id")
+  .get(protect, moderator, getUserById)
+  .put(protect, moderator, updateUserByModerator);
 
 export default router;
